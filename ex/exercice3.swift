57cf9322e9e1576c8f05bcd02bbe167e9f74c8be
jphayek <jphayek1@gmail.com>
2019-11-28 16:56:22 +0100

    mid

--- ex/exercice3.swift
+++ ex/exercice3.swift
@@ -11,9 +11,33 @@ import Foundation
 
 //str.lowercased()
 
-//extension String {
-//    func countAndReturn() -> String {
-//        return "count: \(self.count) string: \(self)"
-//    }
-//}
+extension String {
+    func toDate(format: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        let date = dateFormatter.date(from: self)!
+        return date
+    }
+}
 
+extension Date {
+    func toString(format: String) -> String {
+        let formatter = DateFormatter()
+        // initially set the format based on your datepicker date / server String
+        formatter.dateFormat = "yyyy-MM-dd"
+        // convert your date to string
+        let myStringafd = formatter.string(from: self)
+        
+        return myStringafd
+    }
+    
+    func getYearDifferenceFrom(format: Date) -> Int {
+        let formatter = DateFormatter()
+        // initially set the format based on your datepicker date / server String
+        formatter.dateFormat = "yyyy-MM-dd"
+        // convert your date to string
+        let myStringafd = formatter.string(from: self)
+        
+        return myStringafd
+    }
+}
--- ex/main.swift
+++ ex/main.swift
@@ -8,9 +8,13 @@
 
 import Foundation
 
-print(condog(str: "zhzh", prefix: "s", suffix: "zae"))
+//print(condog(str: "zhzh", prefix: "s", suffix: "zae"))
 
 //print(cleanHobbits(arr: <#T##[String]#>))
+
+print("2000-06-16".toDate(format: "YYYY-MM-DD"))
+
+print(Date().toString(format: "YYYY-MM-DD"))
 
 //let str = "BONJOUR"
 //print(str.countAndReturn())
